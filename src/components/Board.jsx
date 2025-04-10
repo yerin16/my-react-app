85d7fcf30b3492581bfaeae4d3e48036d49cf173
Yerin Kang <61400861+yerin16@users.noreply.github.com>
2025-04-10 16:27:44 -0500

    null array created

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 import Square from './Square'
 
 function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   )
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,20 +1,7 @@
 import React from "react";
-import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
-
-  return (
-    <>
-      <button className="square" onClick={handleClick}>
-        {value}
-      </button>
-    </>
-  );
+function Square({value}) {
+  return <button className="square">{value}</button>;
 }
 
 export default Square;
