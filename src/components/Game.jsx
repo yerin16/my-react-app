cfffe359c890c3b31e3268b70883bdae4c04aa52
Yerin Kang <61400861+yerin16@users.noreply.github.com>
2025-04-10 17:15:44 -0500

    picking a key added

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -23,7 +23,7 @@ function Game() {
       description = "Go to game start";
     }
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
